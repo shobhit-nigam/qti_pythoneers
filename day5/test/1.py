5d6fe0fd76e6fd10b96627d8ae732aed9b6bc809
Shobhit Nigam <50029224+shobhit-nigam@users.noreply.github.com>
2021-10-29 06:38:43 +0000

    Add files via upload

---
+++ day5/test/1.py
@@ -0,0 +1,6 @@
+lista = [1, 2, 3]
+
+assert(sum(lista)) == 6
+
+# assertion error
+assert(sum(lista)) == 7
---
+++ day5/test/3.py
@@ -0,0 +1,14 @@
+import unittest
+import application
+
+lista = [11, 22, 33]
+
+class TestSample(unittest.TestCase):
+    def test_one(self):
+        self.assertEqual(application.add(lista), 66)
+
+    @unittest.skip("will skip this")
+    def test_two(self):
+        self.assertListEqual(lista, [11, 22, 44])
+
+unittest.main()
